21a0e39d76a57361c0c945c59e11dcb120c9e56f
findingsimple <jason@findingsimple.com>
2025-07-18 04:19:13 +0000

    Add plugin file

---
+++ brinybits-custom.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Brinybits.au custom code
+ * Plugin URI:        https://brinybits.au/
+ * Description:       Custom code.
+ * Version:           0.1
+ * Requires at least: 6.0
+ * Requires PHP:      8.0
+ * Author:            findingsimple
+ * Author URI:        https://findingsimple.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/binaryplayground/brinybits-custom/
+ * Text Domain:       brinybits-custom
+ */
+
