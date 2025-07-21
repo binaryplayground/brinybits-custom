ad17e53a86de6cc931433958609762aecff24929
findingsimple <jason@findingsimple.com>
2025-07-21 14:04:42 +0930

    Add login/out menu item

--- brinybits-custom.php
+++ brinybits-custom.php
@@ -22,3 +22,18 @@ function brinybits_add_typekit(){
 font-variation-settings: "ELSH" 3, "RECT" 1, "BACK" 1, "wght" 900; }</style>';
 	}
 };
+
+function add_loginout_block_to_primary_navigation( $hooked_block_types, $relative_position, $anchor_block_type, $context ) {
+
+	// Is $context a Navigation menu?
+	if ( ! $context instanceof WP_Post || 'wp_navigation' !== $context->post_type ) {
+		return $hooked_block_types;
+	}
+	
+	if ( 'last_child' === $relative_position && 'core/navigation' === $anchor_block_type ) {
+		$hooked_block_types[] = 'core/loginout';
+	}
+
+	return $hooked_block_types;
+}
+add_filter( 'hooked_block_types', 'add_loginout_block_to_primary_navigation', 10, 4 );
