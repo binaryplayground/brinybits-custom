6559eab139373ebaae320794da464d7d92699607
findingsimple <jason@findingsimple.com>
2025-07-23 06:27:14 +0930

    Remove login language filter

--- brinybits-custom.php
+++ brinybits-custom.php
@@ -14,14 +14,21 @@
  * Text Domain:       brinybits-custom
  */
 
-add_action('wp_head', 'brinybits_add_typekit');
+/**
+ * Remove login language filter
+ */
+add_filter( 'login_display_language_dropdown', '__return_false' );
 
+/**
+ * Add typekit fonts
+ */
 function brinybits_add_typekit(){
 	if ( ! is_admin() ) { 
 		echo '<link rel="stylesheet" href="https://use.typekit.net/jyb5iaa.css"><style>.wp-block-site-title a { font-family: "gridlite-pe-variable", sans-serif;
 font-variation-settings: "ELSH" 3, "RECT" 1, "BACK" 1, "wght" 900; }</style>';
 	}
 };
+add_action('wp_head', 'brinybits_add_typekit');
 
 /**
  * Changes the logo title (hover text)
